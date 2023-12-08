e99eef28994c1c94f33af4fd4c4a2fff28e30fda
ChunAn Wu <an.wu@canonical.com>
2023-12-08 09:08:08 +0000

    Update the build procedure to build multiple version into one snap
    Signed-off-by: ChunAn Wu <an.wu@canonical.com>

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == "build" ]
 then
-    VERSION=3.19.0 snapcraft --destructive-mode --target-arch=arm64 --enable-experimental-target-arch
+    snapcraft --destructive-mode --target-arch=arm64 --enable-experimental-target-arch
 
 elif [ "$1" == "clean" ]
 then
